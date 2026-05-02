f2668059165ff582c527a5dc03c66304b2161d0d
ydushsanyal-gif <ydushsanyal@gmail.com>
2026-05-02 15:52:22 +0530

    hy docker

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Date currentDate = new Date();
-        System.out.println("Hello, Docker! Current date: " + currentDate);
+        System.out.println("Hy, Docker! Current date: " + currentDate);
     }
 }
